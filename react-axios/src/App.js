c4dd432f23d8d5c0c7492f4081609e5e0e68c8ac
gorkemyagci <gorkemyagci705@gmail.com>
2022-12-19 21:52:50 +0300

    Axios-Get

---
+++ react-axios/src/App.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const getTodos = async () => {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      const data = res.data;
+      setTodos(data);
+    }
+    getTodos();
+    console.log(todos);
+  })
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <div className="todos">
+        {todos.map((todo, index) => {
+          return (
+            <div key={index} style={{
+              padding:"20px"
+            }}>
+              <span>Todo: {todo.title}</span>
+              <input type="checkbox" checked={todo.completed}/>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
